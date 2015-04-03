23c3a80db674288b9abe530f9ff904974dd7fcd1
userhf <userhf@users.noreply.github.com>
2015-04-03 16:47:36 -0700

    Now will delay up to 5:55
    Not confirmed supported

--- README.ino
+++ README.ino
@@ -1,13 +1,16 @@
-boolean stop = false;
+boolean bln;
+double howlong = 0.00027;
+long lng;
 long pitcher = 1000;
 void setup(){
+    howlong = 5; // in hours, how long until the timer goes off? No less than 5 hours, 55 minutes.
+    lng = howlong*3600000;
+    delay(lng);
 }
 void loop() {
 tone (8, pitcher, (1000/8));
     delay (150);
     if (pitcher < 2500){
       pitcher = pitcher + 10;
-      stop = true;
-  
   }
     }
